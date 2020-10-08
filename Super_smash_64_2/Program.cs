2bff4167bacb467d6b4c9263baf10be16257ce92
Lukas <lukas.tuna03@gmail.com>
2020-10-08 15:04:29 +0200

    commit 3 de går inge bra nu :(

--- Super_smash_64_2/Program.cs
+++ Super_smash_64_2/Program.cs
@@ -6,17 +6,28 @@ namespace Super_smash_64_2
     {
         static void Main(string[] args)
         {
+            Random player1dmg = new Random();
+
             Console.WriteLine("Hello World!");
             string player1 = "Lukas";
             string player2 = "Joel";
             int player1hp = 100;
             int player2hp = 100;
+            int player1dmg = (1, 21);
+
+
 
 
             Console.WriteLine("Välkommen till mitt tråkiga spel");
+            while (player1hp != 0 || player2hp != 0)
+            {
+
+            }
 
             Console.ReadLine();
+
         }
+
 
 
 
