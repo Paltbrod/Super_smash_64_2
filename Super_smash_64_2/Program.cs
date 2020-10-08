1ec9463b0e511a92644a0fc07f5b48e6b36206d2
Lukas <lukas.tuna03@gmail.com>
2020-10-08 14:50:27 +0200

    commit 2

--- Super_smash_64_2/Program.cs
+++ Super_smash_64_2/Program.cs
@@ -7,6 +7,24 @@ namespace Super_smash_64_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string player1 = "Lukas";
+            string player2 = "Joel";
+            int player1hp = 100;
+            int player2hp = 100;
+
+
+            Console.WriteLine("Välkommen till mitt tråkiga spel");
+
+            Console.ReadLine();
         }
+
+
+
+
+
+
+
+
     }
+
 }
