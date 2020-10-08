c0fd5a5cb706f70e3f68bc2e821932e0496b53e5
Lukas <lukas.tuna03@gmail.com>
2020-10-08 14:28:28 +0200

    first commit

---
+++ Super_smash_64_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Super_smash_64_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
