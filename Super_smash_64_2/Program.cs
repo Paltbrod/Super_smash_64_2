b53e4750e0acde6d647907400f449ea8429ddd56
Lukas <lukas.tuna03@gmail.com>
2021-02-01 08:24:23 +0100

    mer av samma

--- Super_smash_64_2/Program.cs
+++ Super_smash_64_2/Program.cs
@@ -10,9 +10,11 @@ namespace Super_smash_64_2
 
             string player1 = "Lukas";
             string player2 = "Joel";
+            string player3 = "Vidar";
 
             int player1hp = 100;
             int player2hp = 100;
+            int player3hp = 100;
 
             int r = generator.Next(1, 21);
 
@@ -61,7 +63,81 @@ namespace Super_smash_64_2
 
             }
 
-            else if (chooseChamp != player1 && chooseChamp2 != player2)
+            if (chooseChamp == ("Lukas") && chooseChamp2 == ("Vidar"))
+            {
+                Console.WriteLine("Lukas kommer ju vinna det vet vi redan...Men visst börja slåss");
+
+                while (player1hp >= 0 && player3hp >= 0)
+                {
+
+                    int dmg = generator.Next(10, 20);
+                    int dmg3 = generator.Next(10, 20);
+
+                    player1hp = player1hp - dmg;
+                    player3hp = player3hp - dmg3;
+
+                    Console.WriteLine(player1 + " har nu " + (player1hp) + " hp");
+                    Console.WriteLine(player3 + " har nu " + (player3hp) + " hp");
+                    Console.ReadLine();
+                }
+
+                if (player1hp < 0 && player3hp < 0)
+                {
+                    Console.WriteLine("tråkigt värre det blev oavgjort");
+                    Console.ReadLine();
+                }
+                else if (player1hp >= 0)
+                {
+                    Console.WriteLine("Vilken överaskning " + (player1) + " vann matchen");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine(player3 + " vann. Det måste ha varit någon bug i spelet.");
+                    Console.ReadLine();
+                }
+
+
+            }
+
+            if (chooseChamp == ("Joel") && chooseChamp2 == ("Vidar"))
+            {
+                Console.WriteLine("Joel mot Vidar dehär kan ju inte sluta bra");
+
+                while (player2hp >= 0 && player3hp >= 0)
+                {
+
+                    int dmg = generator.Next(10, 20);
+                    int dmg3 = generator.Next(10, 20);
+
+                    player2hp = player2hp - dmg;
+                    player3hp = player3hp - dmg3;
+
+                    Console.WriteLine(player2 + " har nu " + (player2hp) + " hp");
+                    Console.WriteLine(player3 + " har nu " + (player3hp) + " hp");
+                    Console.ReadLine();
+                }
+
+                if (player2hp < 0 && player3hp < 0)
+                {
+                    Console.WriteLine("tråkigt värre det blev oavgjort");
+                    Console.ReadLine();
+                }
+                else if (player2hp >= 0)
+                {
+                    Console.WriteLine("Wow " + (player2) + " vann matchen");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine(player3 + " vann. Jag blir inte betald nog för att göra dehär...");
+                    Console.ReadLine();
+                }
+
+
+            }
+
+            else if (chooseChamp != player1 && chooseChamp2 != player2 || chooseChamp != player1 && chooseChamp2 != player3 || chooseChamp != player2 && chooseChamp2 != player3)
             {
                 Console.WriteLine("välj någon som man kan spela din nöt");
             }
